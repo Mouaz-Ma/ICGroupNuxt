05cfe50e9fac6fc97ca78a82bb87ac5b19cc769d
mouaz-m <maatouq.45@gmail.com>
2022-03-30 04:26:11 +0300

    hopfully solved the vue build loader deploy issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,9 +54,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    "@nuxtjs/vuetify",    
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [

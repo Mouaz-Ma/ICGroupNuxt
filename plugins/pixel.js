a07e8edf290642d2f15cedb2387c2cb72c13574f
mouaz-ma <mouaz-m@Hotmail.com>
2022-05-10 22:36:07 +0300

    face pixel env

--- plugins/pixel.js
+++ plugins/pixel.js
@@ -6,5 +6,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '143715720571730');
+fbq('init', process.env.PIXIL_INIT);
 fbq('track', 'PageView');

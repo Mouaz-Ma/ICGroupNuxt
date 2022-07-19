8c9d5dd7f92558c9c16e7abe5e8dee6cdc0b283f
mouaz-m <maatouq.45@gmail.com>
2022-07-19 20:23:44 +0300

    meta mask connect wallet

---
+++ components/Metamask.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        @click="$emit('connect')"
+        class="btn-primary metBtn">
+        <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="Metamask"
+            class="h-5 mr-2 metaImage">
+        Connect to Metamask
+    </div>
+</template>
+<style scoped>
+.metaImage{
+    height: 50px;
+}
+.metBtn{
+    width: 300px;
+}
+
+</style>
--- pages/users/profile.vue
+++ pages/users/profile.vue
@@ -269,6 +269,15 @@
 
                         </div>
                     </div>
+                                <div class="p-4">
+                    <Metamask @connect="initWeb3" />
+                    <div>
+                    <p>Network: {{ web3.networkId }}</p>
+                    <p>Account: {{ web3.coinbase }}</p>
+                    <p>Balance: {{ web3.balance }}</p>
+                    </div>
+                    <p class="italic text-red-600">{{ errorMessage }}</p>
+                </div>
                 </div>
 
 <!-- 
@@ -521,43 +530,87 @@
 </v-app>
 </template>
 <script>
+import Web3 from 'web3'
+import {
+  mapGetters,
+  mapMutations
+} from 'vuex';
 export default {
-    data() {
-        return{
-            name:"",
-            email:"",
-            verifyAlertMessage: '',
-            copiedToClipBoard: '',
-             snackbar: false,
-        }
+  data() {
+    return {
+      name: "",
+      email: "",
+      verifyAlertMessage: '',
+      copiedToClipBoard: '',
+      snackbar: false,
+      errorMessage: ''
+    }
+  },
+  computed: {
+    ...mapGetters('web3', ['getInstance']),
+    web3() {
+      return this.getInstance
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout()
     },
-    methods: {
-        async logout() {
-          await this.$auth.logout()
-        },
     async getVerified() {
-        try {
-            const userId = this.$auth.$state.user._id
-            const response = await this.$axios.get('/api/users/varifyById/'+userId)
-            if(response.data.success){
-                this.verifyAlertMessage = 'true'
-            } else {
-                this.verifyAlertMessage = 'false'
-            }
-        } catch(err){
-            this.verifyAlertMessage = 'false'
+      try {
+        const userId = this.$auth.$state.user._id
+        const response = await this.$axios.get('/api/users/varifyById/' + userId)
+        if (response.data.success) {
+          this.verifyAlertMessage = 'true'
+        } else {
+          this.verifyAlertMessage = 'false'
         }
+      } catch (err) {
+        this.verifyAlertMessage = 'false'
+      }
 
     },
-       async copySign(iban) {
-           this.snackbar = true
-        navigator.clipboard.writeText(iban);
+    async copySign(iban) {
+      this.snackbar = true
+      navigator.clipboard.writeText(iban);
 
-        /* Alert the copied text */
-        this.copiedToClipBoard =("Copied the text: " + iban);
-            }
+      /* Alert the copied text */
+      this.copiedToClipBoard = ("Copied the text: " + iban);
     },
-    
+    ...mapMutations('web3', ['registerWeb3Instance']),
+    async initWeb3() {
+      // Check for web3 provider
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          // Ask to connect
+          await window.ethereum.send('eth_requestAccounts');
+          const instance = new Web3(window.ethereum)
+          // Get necessary info on your node
+          const networkId = await instance.eth.net.getId();
+          const coinbase = await instance.eth.getCoinbase();
+          const balance = await instance.eth.getBalance(coinbase);
+          // Save it to store
+          this.registerWeb3Instance({
+            networkId,
+            coinbase,
+            balance
+          });
+          this.errorMessage = '';
+        } catch (error) {
+          // User denied account access
+          console.error('User denied web3 access', error);
+          this.errorMessage = 'Please connect to your Ethereum address on Metamask before connecting to this website';
+          return;
+        }
+      }
+      // No web3 provider
+      else {
+        console.error('No web3 provider detected');
+        this.errorMessage = "No web3 provider detected. Did you install the Metamask extension on this browser?";
+        return;
+      }
+    },
+  },
 }
 </script>
 

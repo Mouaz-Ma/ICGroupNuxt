653219fa75cc3b221c61cad29818a44030fcc2c6
SamiAssasa <sami.assasa.dev@gmail.com>
2022-08-12 13:51:54 +0300

    fix tickertape component.
    the fix was to add TradingView script dynamically to avoid side effects.
    The other thing that TradingView was expecting a stringified JSON object
    inside the script tag.

--- components/tickertape.vue
+++ components/tickertape.vue
@@ -1,58 +1,73 @@
 <template>
 
-<div class="tradingview-widget-container">
+<div class="tradingview-widget-container" id="tradingview-ticker">
   <div class="tradingview-widget-container__widget"></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-  {
-  "symbols": [
-    {
-      "proName": "BITSTAMP:BTCUSD",
-      "title": "Bitcoin"
-    },
-    {
-      "proName": "BITSTAMP:ETHUSD",
-      "title": "Ethereum"
-    },
-    {
-      "description": "GOLD",
-      "proName": "CURRENCYCOM:GOLD"
-    },
-    {
-      "description": "USD/TRY",
-      "proName": "CURRENCYCOM:USDTRY"
-    },
-    {
-      "description": "EUR/TRY",
-      "proName": "CURRENCYCOM:EURTRY"
-    },
-    {
-      "description": "GBP/TRY",
-      "proName": "CURRENCYCOM:GBPTRY"
-    },
-    {
-      "description": "JPY/TRY",
-      "proName": "FX_IDC:JPYTRY"
-    },
-    {
-      "description": "USD/SYP",
-      "proName": "FX_IDC:USDSYP"
-    },
-    {
-      "description": "USD/EUR",
-      "proName": "FX_IDC:USDEUR"
-    },
-    {
-      "description": "USD/EUR",
-      "proName": "FX_IDC:USDEUR"
-    },
-  ],
-  "showSymbolLogo": true,
-  "colorTheme": "light",
-  "isTransparent": true,
-  "displayMode": "regular",
-  "locale": "en"
-}
-  </script>
 </div>
     
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      //
+    }
+  },
+  mounted() {
+    let tradingWidgetContainer = document.getElementById("tradingview-ticker");
+    let tradingWidgetScript = document.createElement("script");
+    tradingWidgetScript.src = "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
+    tradingWidgetScript.type = "text/javascript";
+    tradingWidgetScript.text = JSON.stringify({
+    "symbols": [
+      {
+        "proName": "BITSTAMP:BTCUSD",
+        "title": "Bitcoin"
+      },
+      {
+        "proName": "BITSTAMP:ETHUSD",
+        "title": "Ethereum"
+      },
+      {
+        "description": "GOLD",
+        "proName": "CURRENCYCOM:GOLD"
+      },
+      {
+        "description": "USD/TRY",
+        "proName": "CURRENCYCOM:USDTRY"
+      },
+      {
+        "description": "EUR/TRY",
+        "proName": "CURRENCYCOM:EURTRY"
+      },
+      {
+        "description": "GBP/TRY",
+        "proName": "CURRENCYCOM:GBPTRY"
+      },
+      {
+        "description": "JPY/TRY",
+        "proName": "FX_IDC:JPYTRY"
+      },
+      {
+        "description": "USD/SYP",
+        "proName": "FX_IDC:USDSYP"
+      },
+      {
+        "description": "USD/EUR",
+        "proName": "FX_IDC:USDEUR"
+      },
+      {
+        "description": "USD/EUR",
+        "proName": "FX_IDC:USDEUR"
+      },
+    ],
+    "showSymbolLogo": true,
+    "colorTheme": "light",
+    "isTransparent": true,
+    "displayMode": "regular",
+    "locale": "en"
+});
+    tradingWidgetContainer.appendChild(tradingWidgetScript);
+  }
+}
+</script>

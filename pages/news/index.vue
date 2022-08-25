e52e1dc699e91b790c4d5e69e406a34a74d528fa
SamiAssasa <sami.assasa.dev@gmail.com>
2022-08-25 10:14:45 +0300

    fix news page cards layout

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
-    <b-card-group class="d-flex">
-      <div v-for="news in allNewsData" :key="news._id" class="col-xl-4 col-lg-4 col-md-4">
+    <b-card-group class="">
+      <div v-for="news in allNewsData" :key="news._id" class="col-12 col-sm-6 col-md-4">
         <news-card style="width:100%;" :newsId=news._id :title=news.title author="By: ICGroupsFX" :content=news.content
           :imageSrc=news.image.url :createdAt=news.createdAt />
       </div>

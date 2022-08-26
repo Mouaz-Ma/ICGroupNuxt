53d42c3a0805d01329cd0fa11306afce471871c2
SamiAssasa <sami.assasa.dev@gmail.com>
2022-08-26 16:15:02 +0300

    update privacy page with en/ar content.

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,109 +1,318 @@
 <template>
-    <div id="main-wrapper show">
+  <div id="main-wrapper show">
     <Banner title="Privacy Policy" />
     <div class="terms_condition">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-9">
-                    <div class="terms_condition-content">
-                        <div class="terms_condition-text">
-                            <h3>Your privacy is important to us: </h3>
-                            <p>Therefore, we guarantee that:</p>
-                            <ul>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>We do not rent or sell your personal information to anyone.</p>
-                                </li>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>Any personal information you provide will be secured by us.</p>
-                                </li>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>You will be able to erase all the data we have stored on you at any given
-                                        time. To request data termination, please contact our customer support.</p>
-                                </li>
-                            </ul>
-                        </div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-9">
+            <div class="terms_condition-content">
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy introduction') }}</h3>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy introduction i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy introduction i2') }}
+                  </li>
+                </ol>
+              </div>
 
 
-                        <div class="terms_condition-text">
-                            <h3>Third-party services: </h3>
-                            <p>We use third-party services in order to operate our website. Please note that these
-                                services may contain links to third-party apps, websites or services that are not
-                                operated by us. We make no representation or warranties
-                                with regard to and are not responsible for the content, functionality, legality,
-                                security, accuracy, or other aspects of such third-party apps, websites or services.
-                                Note that, when accessing and/or using these third-party
-                                services, their own privacy policy may apply.</p>
-                        </div>
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy collecting personal information') }}</h3>
+                <p>{{ $t('privacy collecting personal information p1') }}</p>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy collecting personal information i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i2') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i3') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i4') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i5') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i6') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i7') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i8') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i9') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy collecting personal information i10') }}
+                  </li>
+                </ol>
+                <p>{{ $t('privacy collecting personal information p2') }}</p>
+              </div>
 
-                        <div class="terms_condition-text">
-                            <h3>Google Analytics: </h3>
-                            <p>This website uses Google Analytics, a web analytics service provided by Google, Inc.
-                                (“Google”). Google Analytics uses “cookies”, which are text files placed on your
-                                computer, to help the website analyze how users use the
-                                site. The information generated by the cookie about your use of the website will be
-                                transmitted to and stored by Google on servers in the United States . In case
-                                IP-anonymisation is activated on this website, your IP
-                                address will be truncated within the area of Member States of the European Union or
-                                other parties to the Agreement on the European Economic Area. Only in exceptional
-                                cases the whole IP address will be first transferred
-                                to a Google server in the USA and truncated there. The IP-anonymisation is active on
-                                this website. Google will use this information on behalf of the operator of this
-                                website for the purpose of evaluating your use of
-                                the website, compiling reports on website activity for website operators and
-                                providing them other services relating to website activity and internet usage. The
-                                IP-address, that your Browser conveys within the scope
-                                of Google Analytics, will not be associated with any other data held by Google. You
-                                may refuse the use of cookies by selecting the appropriate settings on your browser,
-                                however please note that if you do this you may
-                                not be able to use the full functionality of www.Tradix.com. You can also opt-out
-                                from being tracked by Google Analytics with effect for the future by downloading and
-                                installing Google Analytics Opt-out Browser Addon
-                                for your current web browser: https://tools.google.com/dlpage/gaoptout?hl=en.
-                            </p>
-                        </div>
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy using your personal information') }}</h3>
+                <p>{{ $t('privacy using your personal information p1') }}</p>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy using your personal information i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i2') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i3') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i4') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i5') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i6') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i7') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i8') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i9') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i10') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i11') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i12') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy using your personal information i13') }}
+                  </li>
+                </ol>
+                <p>{{ $t('privacy using your personal information p2') }}</p>
+                <p>{{ $t('privacy using your personal information p3') }}</p>
+                <p>{{ $t('privacy using your personal information p4') }}</p>
+              </div>
 
-                        <div class="terms_condition-text">
-                            <h3>Information we collect: </h3>
-                            <p>Information we collect: </p>
-                            <ul>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>Google ID (to identify you in our database)</p>
-                                </li>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>Google First & Last name</p>
-                                </li>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>Google Email</p>
-                                </li>
-                                <li>
-                                    <i class="fa fa-circle"></i>
-                                    <p>Google avatar image</p>
-                                </li>
-                            </ul>
-                            <p>We do not collect passwords or any other sensitive information.</p>
-                        </div>
-                    </div>
-                </div>
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy disclosing personal information') }}</h3>
+                <p>{{ $t('privacy disclosing personal information p1') }}</p>
+                <p>{{ $t('privacy disclosing personal information p2') }}</p>
+                <p>{{ $t('privacy disclosing personal information p3') }}</p>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy disclosing personal information i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy disclosing personal information i2') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy disclosing personal information i3') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy disclosing personal information i4') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy disclosing personal information i5') }}
+                  </li>
+                </ol>
+                <p>{{ $t('privacy disclosing personal information p4') }}</p>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy international data transfers') }}</h3>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy international data transfers i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy international data transfers i2') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy international data transfers i3') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy international data transfers i4') }}
+                  </li>
+                </ol>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy retaining personal information') }}</h3>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy retaining personal information i1') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy retaining personal information i2') }}
+                  </li>
+                  <li>
+                    {{ $t('privacy retaining personal information i3') }}
+                    <ol class="list-group-numbered">
+                      <li>
+                        {{ $t('privacy retaining personal information i3-i1') }}
+                      </li>
+                      <li>
+                        {{ $t('privacy retaining personal information i3-i2') }}
+                      </li>
+                      <li>
+                        {{ $t('privacy retaining personal information i3-i3') }}
+                      </li>
+                    </ol>
+                  </li>
+                </ol>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy security of your personal information') }}</h3>
+                <ol>
+                  <li>
+                    <p>{{ $t('privacy security of your personal information i1') }}</p>
+                  </li>
+                  <li>
+                    <p>{{ $t('privacy security of your personal information i2') }}</p>
+                  </li>
+                  <li>
+                    <p>{{ $t('privacy security of your personal information i3') }}</p>
+                  </li>
+                  <li>
+                    <p>{{ $t('privacy security of your personal information i4') }}</p>
+                  </li>
+                  <li>
+                    <p>{{ $t('privacy security of your personal information i5') }}</p>
+                  </li>
+                </ol>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy amendments') }}</h3>
+                <p>{{ $t('privacy amendments p1') }}</p>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy your rights') }}</h3>
+                <p>{{ $t('privacy your rights p1') }}</p>
+                <p>{{ $t('privacy your rights p2') }}</p>
+                <p>{{ $t('privacy your rights p3') }}</p>
+                <p>{{ $t('privacy your rights p4') }}</p>
+                <p>{{ $t('privacy your rights p5') }}</p>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy third-party websites') }}</h3>
+                <p>{{ $t('privacy third-party websites p1') }}</p>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy updating information') }}</h3>
+                <p>{{ $t('privacy updating information p1') }}</p>
+              </div>
+
+              <div class="terms_condition-text">
+                <h3>{{ $t('privacy cookies') }}</h3>
+                <p>{{ $t('privacy cookies p1') }}</p>
+                <ol class="list-group-numbered">
+                  <li>
+                    {{ $t('privacy cookies i1') }}
+                    <ol class="list-group-numbered">
+                      <li>
+                        {{ $t('privacy cookies i1-i1') }}
+                      </li>
+                    </ol>
+                  </li>
+                  <li>
+                    {{ $t('privacy cookies i2') }}
+                    <ol class="list-group-numbered">
+                      <li>
+                        {{ $t('privacy cookies i2-i1') }}
+                      </li>
+                      <li>
+                        {{ $t('privacy cookies i2-i2') }}
+                      </li>
+                      <li>
+                        {{ $t('privacy cookies i2-i3') }}
+                      </li>
+                    </ol>
+                  </li>
+                  <li>
+                    {{ $t('privacy cookies i3') }}
+                  </li>
+                </ol>
+                <p>{{ $t('privacy cookies p2') }}</p>
+              </div>
+
             </div>
+          </div>
         </div>
+      </div>
     </div>
 
-</div>
+  </div>
 </template>
+
 <script>
-import Banner from "@/components/pageBanner";
+import Banner from '@/components/pageBanner';
 export default {
-      auth: false,
-        scrollToTop: true,
-          layout: 'index',
-          components: {
-            Banner
-          },
-        }
+  auth: false,
+  scrollToTop: true,
+  layout: 'index',
+  components: {
+    Banner,
+  },
+};
 </script>
+
+<style>
+/* .terms_condition-text ol {
+    counter-reset: item;
+} */
+
+.terms_condition {
+  counter-reset: h3;
+}
+
+.terms_condition-text h3 {
+  counter-increment: h3;
+}
+
+html:lang(en) .terms_condition-text h3::before {
+  content: counter(h3, upper-alpha) '.';
+  margin-right: 0.5rem;
+}
+
+html:lang(ar) .terms_condition-text h3::before {
+  content: counter(h3, upper-alpha) '.';
+  margin-left: 0.5rem;
+}
+
+.terms_condition-text li {
+  display: block;
+  font-size: 16px;
+  margin-bottom: 16px;
+}
+
+/* .terms_condition-text ol > li {
+    counter-increment: item;
+}
+
+.terms_condition-text ol ol > li {
+    display: block;
+}
+
+.terms_condition-text ol ol > li:before {
+    content: counters(item, ".") ". ";
+    margin-left: -20px;
+} */
+</style>

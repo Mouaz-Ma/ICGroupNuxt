d21fce06917c69cdb7477b3829a4470b08cc8b6d
HanasakiShizue <hanasaki.shizue.11@gmail.com>
2022-09-13 19:17:41 +0300

    translate user information card component

--- components/UserAdminUploads.vue
+++ components/UserAdminUploads.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="icg-card" v-if="$auth.$state.user.userType === 'Administrator'">
     <div class="icg-card__header">
-      <h4 class="card-title">Uploads</h4>
+      <h4 class="card-title">{{$t("Uploads")}}</h4>
     </div>
     <div class="card-body">
       <div class="app-link">
-        <h5>What would you like to upload</h5>
+        <h5>{{$t("What would you like to upload?")}}</h5>
         <div class="d-flex flex-row justify-content-between align-items-center">
-          <nuxt-link class="btn btn-primary" to="/blogs/uploadBlog">Blog</nuxt-link>
-          <nuxt-link class="btn btn-primary" to="/analysis/uploadAnalysis">Analysis</nuxt-link>
-          <nuxt-link class="btn btn-primary" to="/news/uploadNews">News</nuxt-link>
+          <nuxt-link class="btn btn-primary" to="/blogs/uploadBlog">{{$t("Blog")}}</nuxt-link>
+          <nuxt-link class="btn btn-primary" to="/analysis/uploadAnalysis">{{$t("Analysis")}}</nuxt-link>
+          <nuxt-link class="btn btn-primary" to="/news/uploadNews">{{$t("News")}}</nuxt-link>
         </div>
       </div>
     </div>
--- components/UserInformationCard.vue
+++ components/UserInformationCard.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="icg-card">
     <div class="icg-card__header">
-      <h4 class="card-title">Information</h4>
-      <nuxt-link to="/users/profileUpdate" class="btn btn-primary">Edit</nuxt-link>
+      <h4 class="card-title">{{$t("Information")}}</h4>
+      <nuxt-link to="/users/profileUpdate" class="btn btn-primary">{{$t("Edit")}}</nuxt-link>
     </div>
     <div class="card-body">
       <form class="row">
         <div class="col-12 col-md-6 col-lg-6">
           <div class="user-info">
-            <span>USER ID</span>
+            <span>{{$t("USER ID")}}</span>
             <h4>{{$auth.$state.user._id}}</h4>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-6">
           <div class="user-info">
-            <span>EMAIL ADDRESS</span>
+            <span>{{$t("EMAIL ADDRESS")}}</span>
             <h4>{{$auth.$state.user.email}}</h4>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-6">
           <div class="user-info">
-            <span>TELEPHONE</span>
+            <span>{{$t("TELEPHONE")}}</span>
             <h4>{{$auth.$state.user.telephone}}</h4>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-6">
           <div class="user-info">
-            <span>USER TYPE</span>
+            <span>{{$t("USER TYPE")}}</span>
             <h4>{{$auth.$state.user.userType}}</h4>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-6">
           <div class="user-info">
-            <span>ACCOUNT STATUS</span>
+            <span>{{$t("ACCOUNT STATUS")}}</span>
             <h4>{{isVerified}}</h4>
           </div>
         </div>

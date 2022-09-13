045943fdae486ee842f6f2adef8910b4b415e649
HanasakiShizue <hanasaki.shizue.11@gmail.com>
2022-09-13 19:56:47 +0300

    fix user profile search bar direction

--- components/Searchbar.vue
+++ components/Searchbar.vue
@@ -1,12 +1,22 @@
 <template>
   <!-- SEARCH BAR BEGIN -->
-  <div class="search">
+  <div class="search" dir="ltr">
     <form action="#">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search Here">
+        <input type="text" class="form-control" :placeholder="placeholder">
         <span class="input-group-text"><i class="icofont-search"></i></span>
       </div>
     </form>
   </div>
   <!-- SEARCH BAR END -->
 </template>
+
+<script>
+export default {
+  computed: {
+    placeholder() {
+      return this.$i18n.locale === 'ar' ? 'البحث' : 'Search';
+    },
+  },
+};
+</script>

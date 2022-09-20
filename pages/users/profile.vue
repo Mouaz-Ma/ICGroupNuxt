bc2216d7e2e88bdde9b36ae1d5db2643ddd27db0
mouaz-ma <mouaz-m@Hotmail.com>
2022-09-20 12:37:42 +0300

    moved get verified

--- pages/users/profile.vue
+++ pages/users/profile.vue
@@ -79,19 +79,6 @@ export default {
     async logout() {
       await this.$auth.logout();
     },
-    async getVerified() {
-      try {
-        const userId = this.$auth.$state.user._id;
-        const response = await this.$axios.get('/api/users/varifyById/' + userId);
-        if (response.data.success) {
-          this.verifyAlertMessage = 'true';
-        } else {
-          this.verifyAlertMessage = 'false';
-        }
-      } catch (err) {
-        this.verifyAlertMessage = 'false';
-      }
-    },
     async copySign(iban) {
       this.snackbar = true;
       navigator.clipboard.writeText(iban);

ce50ca85eec70bc8ca0495871777dc18f1b10644
HanasakiShizue <hanasaki.shizue.11@gmail.com>
2022-09-26 16:22:37 +0300

    fix facebook userinfo endpoint in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,83 +1,83 @@
 import redirectSSL from 'redirect-ssl';
 export default {
-  ssr: true,
+  'ssr': true,
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
+  'head': {
     title: 'ICGroupsFx - للوساطة المالية',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: ''
-      },
-      {
-        name: 'format-detection',
-        content: 'telephone=no'
-      }
+      charset: 'utf-8',
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+    {
+      hid: 'description',
+      name: 'description',
+      content: '',
+    },
+    {
+      name: 'format-detection',
+      content: 'telephone=no',
+    },
     ],
     link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css',
+    },
+    // Iconfonts for Vuetify. You need to leave only which one you use
+    {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons'},
+    {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'},
+    {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.4.95/css/materialdesignicons.min.css'},
+    ],
+    script: [
+      {
+        src: 'https://maps.googleapis.com/maps/api/js?key='+ process.env.GOOGLE_MAP_API_KEY,
+        hid: 'map',
+        defer: true,
       },
       {
-        rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'
+        src: '/assets/js/pixel.js',
+        type: 'text/javascript',
+        hid: 'facebookPixel',
+        ssr: false,
       },
-      // Iconfonts for Vuetify. You need to leave only which one you use
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.4.95/css/materialdesignicons.min.css' }
+      {
+        src: 'https://s3.tradingview.com/tv.js',
+        type: 'text/javascript',
+        hid: 'tradingViewAdvanced',
+        ssr: false,
+      },
     ],
-      script: [
-        {
-        src: "https://maps.googleapis.com/maps/api/js?key="+ process.env.GOOGLE_MAP_API_KEY,
-        hid: "map", 
-        defer: true,
-    },
-    {
-      src:'/assets/js/pixel.js', 
-      type: 'text/javascript',
-      hid: 'facebookPixel',
-      ssr: false
-    },
-    {
-      src:'https://s3.tradingview.com/tv.js', 
-      type: 'text/javascript',
-      hid: 'tradingViewAdvanced',
-      ssr: false
-    }
-  ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
+  'css': [
     '@assets/css/style.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+  'plugins': [
     '~/plugins/direction-control',
-    "~/plugins/TiptapVuetify",
+    '~/plugins/TiptapVuetify',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  'components': true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  'buildModules': [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+  'modules': [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     'nuxt-i18n',
@@ -87,40 +87,40 @@ export default {
     '@nuxtjs/auth',
     '@nuxtjs/fontawesome',
     '@nuxtjs/moment',
-    "@nuxtjs/vuetify",    
+    '@nuxtjs/vuetify',
     '@nuxtjs/google-fonts',
-    '@nuxtjs/google-gtag'
+    '@nuxtjs/google-gtag',
   ],
-  fontawesome: {
+  'fontawesome': {
     icons: {
-      regular: ["faEnvelope"],
+      regular: ['faEnvelope'],
       brands: [
-        "faGoogle",
-        "faFacebook",
-        "faFacebookF",
-        "faFacebookMessenger",
-        "faInstagram",
-        "faTwitter",
-        "faWhatsapp",
-        "faPinterest",
-        "faTelegramPlane"
-      ]
-    }
+        'faGoogle',
+        'faFacebook',
+        'faFacebookF',
+        'faFacebookMessenger',
+        'faInstagram',
+        'faTwitter',
+        'faWhatsapp',
+        'faPinterest',
+        'faTelegramPlane',
+      ],
+    },
   },
-// example config
-'google-gtag':{
-  id: 'UA-'+process.env.GOOGLE_TAG_MANAGER_ID, // required
-  config:{
+  // example config
+  'google-gtag': {
+    id: 'UA-'+process.env.GOOGLE_TAG_MANAGER_ID, // required
+    config: {
     // this are the config options for `gtag
     // check out official docs: https://developers.google.com/analytics/devguides/collection/gtagjs/
-    anonymize_ip: true, // anonymize IP 
-    send_page_view: false, // might be necessary to avoid duplicated page track on page reload
-    linker:{
-      domains:['icgroupsfx.com','icgroup.herokuapp.com']
-    }
-  },
-  debug: true, // enable to track in dev mode
-  disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...)
+      anonymize_ip: true, // anonymize IP
+      send_page_view: false, // might be necessary to avoid duplicated page track on page reload
+      linker: {
+        domains: ['icgroupsfx.com', 'icgroup.herokuapp.com'],
+      },
+    },
+    debug: true, // enable to track in dev mode
+    disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...)
   // optional you can add more configuration like [AdWords](https://developers.google.com/adwords-remarketing-tag/#configuring_the_global_site_tag_for_multiple_accounts)
   // additionalAccounts:[{
   //   id: 'AW-XXXX-XX', // required if you are adding additional IDs
@@ -128,53 +128,53 @@ export default {
   //     send_page_view:false // optional configurations
   //   }
   // }]
- },
-  googleFonts: {
+  },
+  'googleFonts': {
     families: {
       // a simple name
-      Roboto: true,
-      Mostserrat: true,
-  
+      'Roboto': true,
+      'Mostserrat': true,
+
       // a name with spaces
       'Mostserrat+Sans': true,
-  
+
       // specific font weights
-      regular: [100, 300, 400],
-    }
+      'regular': [100, 300, 400],
+    },
   },
 
-  moment: {
-    timezone: true
+  'moment': {
+    timezone: true,
   },
-  bootstrapVue: {
+  'bootstrapVue': {
     // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
-    icons: true
+    icons: true,
   },
-  serverMiddleware: [redirectSSL.create({enabled: process.env.NODE_ENV === 'production'})],
-  i18n: {
+  'serverMiddleware': [redirectSSL.create({enabled: process.env.NODE_ENV === 'production'})],
+  'i18n': {
     locales: [
       {
         code: 'en',
         name: 'English',
         iso: 'en-US',
         file: 'en.js',
-        dir: 'ltr'
-      },{
+        dir: 'ltr',
+      }, {
         code: 'ar',
         name: 'العربية',
         iso: 'ae-AE',
         file: 'ar.js',
-        dir: 'rtl'
+        dir: 'rtl',
       },
     ],
-    langDir: "static/lang",
+    langDir: 'static/lang',
     lazy: true,
     defaultLocale: 'en',
     strategy: 'no_prefix',
     silentTranslationWarn: process.env.NODE_ENV === 'production',
     silentFallbackWarn: process.env.NODE_ENV === 'production',
   },
-  axios: {
+  'axios': {
     proxy: true,
     BaseURL: process.env.API_URL,
 
@@ -182,33 +182,33 @@ export default {
     // credentials: false
   },
 
-  proxy: {
-    "/api": process.env.API_URL
+  'proxy': {
+    '/api': process.env.API_URL,
   },
-//   server: {
-//     host: "192.168.254.246",
-//     port: 3000,
-// },
+  //   server: {
+  //     host: "192.168.254.246",
+  //     port: 3000,
+  // },
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: ['vuetify/lib', "tiptap-vuetify"],
+  'build': {
+    transpile: ['vuetify/lib', 'tiptap-vuetify'],
     babel: {
       compact: true,
-     },
+    },
   },
-  router: {
-    middleware: ['auth']
+  'router': {
+    middleware: ['auth'],
   },
 
 
-  auth: {
+  'auth': {
     strategies: {
       local: {
         endpoints: {
           login: {
             url: '/api/users/login',
             method: 'post',
-            propertyName: "token"
+            propertyName: 'token',
           },
           logout: {
             url: '/api/users/logout',
@@ -217,24 +217,24 @@ export default {
           user: {
             url: '/api/users/user',
             method: 'get',
-          }
-        }
+          },
         },
-        facebook: {
-          client_id: '332127222262076',
-          userinfo_endpoint: 'https://graph.facebook.com/me?fields=about,name,picture.typr(large){url},email,birthday',
-          scope: ['public_profile', 'email', 'user_birthday']
-        },
-        google: {
-          client_id: '160001613518-8qdslgbi9iq1h2f01p31utp6mpd2scd5.apps.googleusercontent.com'
-        }
       },
+      facebook: {
+        client_id: '332127222262076',
+        userinfo_endpoint: 'https://graph.facebook.com/me?fields=about,name,picture.width(640){url},email,birthday',
+        scope: ['public_profile', 'email', 'user_birthday'],
+      },
+      google: {
+        client_id: '160001613518-8qdslgbi9iq1h2f01p31utp6mpd2scd5.apps.googleusercontent.com',
+      },
+    },
 
-      redirect: {
-        login: '/users/signIn',
-        logout: '/',
-        callback: '/users/signIn',
-        home: '/'
-      },
-          }
-}
+    redirect: {
+      login: '/users/signIn',
+      logout: '/',
+      callback: '/users/signIn',
+      home: '/',
+    },
+  },
+};
